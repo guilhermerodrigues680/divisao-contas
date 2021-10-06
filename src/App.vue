7a7572ddc98c835b1c6a0219fad7bcfb01fefcc4
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-10-06 11:46:16 -0300

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <div id="app">
-    <Home />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Home from "@/views/home/Home.vue";
-
 export default {
   name: "App",
-
-  components: {
-    Home,
-  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/styles/global.scss";
+import router from './router'
 
 Vue.config.productionTip = false;
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 document.body.ontouchstart = () => {};
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount("#app");
